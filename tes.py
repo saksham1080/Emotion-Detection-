7a99b6c46db11539ce6dbbf9e7a47e5b05ee7a3b
saksham1080 <118593498+saksham1080@users.noreply.github.com>
2024-12-11 17:50:52 +0530

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+print("TensorFlow version:", tf.__version__)
+print("Keras version:", tf.keras.__version__)
+import ml_dtypes
+print("ml-dtypes version:", ml_dtypes.__version__)
+import tensorboard
+print("TensorBoard version:", tensorboard.__version__)
